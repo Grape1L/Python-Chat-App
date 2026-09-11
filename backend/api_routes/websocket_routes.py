ea5b93ec38dd9bc3803ec4f298801e08613d6bbf
Grape1L <ceark123@gmail.com>
2026-09-12 01:35:13 +0200

    Refactor websocket endpoint to improve connection handling and message saving logic

--- backend/api_routes/websocket_routes.py
+++ backend/api_routes/websocket_routes.py
@@ -15,20 +15,26 @@ async def websocket_endpoint(websocket: WebSocket, db: DB = Depends(get_db_ws)):
 
     token = websocket.cookies.get("access_token")
 
-
-    await ws_manager.connect(websocket, token)
+    if not await ws_manager.connect(websocket, token):
+        return
 
     try:
         while True:
             data = await websocket.receive_json()
             target_user_ID = data.get("targetUser_ID")
 
+            sender_id = ws_manager.connected_clients.get_user_id(websocket)
+
+            if not db.are_friends(sender_id, target_user_ID):
+                await ws_manager.disconnect(websocket, 1008)
+                return
+
             # Check if the message should be saved in the database
             disappear: bool = data.get("disappear")
 
-            if disappear == False:
+            if not disappear:
                 db.save_message(
-                    sender_id=ws_manager.connected_clients.get_user_id(websocket), 
+                    sender_id=sender_id, 
                     recipient_id=target_user_ID, 
                     content=data.get("content")
                 )
