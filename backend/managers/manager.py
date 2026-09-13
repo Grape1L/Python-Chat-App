e9b3e1a3ff29645acee4b3846c5025615c8ba88c
Grape1L <ceark123@gmail.com>
2026-09-14 01:04:45 +0200

    Refactor WebSocket management to use async methods and improve token verification handling

--- backend/managers/manager.py
+++ backend/managers/manager.py
@@ -1,34 +1,41 @@
 from fastapi import WebSocket
 from typing import Dict
 from backend.auth import auth_service
-from backend.database.database_control import DB
 import asyncio
 import logging
+from backend.exceptions.token import TokenVerification, InvalidToken
+from fastapi import HTTPException
 
 class ConnectedClients:
     def __init__(self):
         self._id_to_ws: Dict[int, WebSocket] = {}
         self._ws_to_id: Dict[WebSocket, int] = {}
+        self._lock = asyncio.Lock()
 
-    def add_client(self, user_id: int, websocket: WebSocket):
+    async def add_client(self, user_id: int, websocket: WebSocket):
         if type(user_id) is not int:
             raise ValueError("user_id must be int")
         
-        if self._id_to_ws.get(user_id) is not None:
-            self.remove_by_id(user_id)
-        elif self._ws_to_id.get(websocket) is not None:
-            self.remove_by_ws(websocket)
+        async with self._lock:
+            if self._id_to_ws.get(user_id) is not None:
+                old_websocket = self._id_to_ws.pop(user_id, None)
+                self._ws_to_id.pop(old_websocket, None)
+            elif self._ws_to_id.get(websocket) is not None:
+                old_user_id = self._ws_to_id.pop(websocket, None)
+                self._id_to_ws.pop(old_user_id, None)
 
-        self._id_to_ws[user_id] = websocket
-        self._ws_to_id[websocket] = user_id
+            self._id_to_ws[user_id] = websocket
+            self._ws_to_id[websocket] = user_id
 
-    def remove_by_id(self, user_id: int):
-        websocket = self._id_to_ws.pop(user_id, None)
-        self._ws_to_id.pop(websocket, None)
+    async def remove_by_id(self, user_id: int):
+        async with self._lock:
+            websocket = self._id_to_ws.pop(user_id, None)
+            self._ws_to_id.pop(websocket, None)
 
-    def remove_by_ws(self, websocket: WebSocket):
-        user_id = self._ws_to_id.pop(websocket, None)
-        self._id_to_ws.pop(user_id, None)
+    async def remove_by_ws(self, websocket: WebSocket):
+        async with self._lock:
+            user_id = self._ws_to_id.pop(websocket, None)
+            self._id_to_ws.pop(user_id, None)
 
     def get_websocket(self, user_id: int) -> WebSocket | None:
         return self._id_to_ws.get(user_id)
@@ -36,66 +43,58 @@ class ConnectedClients:
     def get_user_id(self, websocket: WebSocket) -> int | None:
         return self._ws_to_id.get(websocket)
     
-    def get_all(self) -> Dict[int, WebSocket]:
-        return self._id_to_ws
-    
     def __len__(self) -> int:
         return len(self._id_to_ws)
     
 
 
 class WebSocketManager:
-    def __init__(self):
-        self.connected_clients = ConnectedClients()
-        self._lock = asyncio.Lock()
+    def __init__(self, client_websocket: WebSocket, connected_clients: ConnectedClients):
+        self.client_user_id = None
+        self.client_websocket = client_websocket
+        self.connected_clients = connected_clients
 
-    async def connect(self, websocket: WebSocket, token) -> bool:
+    async def connect(self, token):
         try:
             user = auth_service.verify_token(token)
-        except Exception:
-            print("WebSocket connection rejected: token verification raised")
-            return False
+        except HTTPException:
+            raise TokenVerification("Token verification failed")
 
         if not user:
-            print("WebSocket connection rejected: Invalid token")
-            return False
-
-        async with self._lock:
-            self.connected_clients.add_client(int(user.get("id")), websocket)
-
-        await websocket.accept()
+            raise InvalidToken("Invalid Token")
         
-        return True
+        self.client_user_id = int(user.get("id"))
+        
+        await self.client_websocket.accept()
+        await self.connected_clients.add_client(self.client_user_id, self.client_websocket)
 
 
-    async def send_message(self, sender_websocket: WebSocket, target_websocket: WebSocket, data: dict, db: DB):
-        sender_uid = self.connected_clients.get_user_id(sender_websocket)
-        if sender_uid is None:
-            print("WebSocket not found in connected clients")
-            return
+    async def send_to_client(self, data: dict):
+        await self.client_websocket.send_json(data)
 
-        user = db.get_user_by_id(sender_uid)
-        if not user:
-            print("User not found")
-            return
+    async def send_error(self, message: str):
+        await self.send_to_client({
+            "type": "error",
+            "message": message
+        })
 
+    async def send_message(self, sender_id, user, target_websocket: WebSocket, data: dict):
         username = user[1]
 
         full_message = {
-            "id": sender_uid,
+            "id": sender_id,
             "user": username,
             **data
         }
 
         await target_websocket.send_json(full_message)
 
-    async def disconnect(self, websocket: WebSocket, code: int = 1000):
-        async with self._lock:
-            self.connected_clients.remove_by_ws(websocket)
+    async def disconnect(self, code: int = 1000):
+        await self.connected_clients.remove_by_ws(self.client_websocket)
 
         try:
-            await websocket.close(code=code)
-        except Exception:
-            print("Error closing websocket during disconnecting")
+            await self.client_websocket.close(code=code)
+        except RuntimeError:
+            pass
             
-        print(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!client disconnected - {websocket.client.host}:{websocket.client.port}")
+        # print(f"client disconnected - {websocket.client.host}:{websocket.client.port}")
